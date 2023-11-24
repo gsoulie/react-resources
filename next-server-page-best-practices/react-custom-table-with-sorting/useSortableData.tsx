28c16f817ae4b0b103a36a759be37e4b2d96e52c
gsoulie <gsoulie@users.noreply.github.com>
2023-11-24 16:32:44 +0100

    Update useSortableData.tsx

--- next-server-page-best-practices/react-custom-table-with-sorting/useSortableData.tsx
+++ next-server-page-best-practices/react-custom-table-with-sorting/useSortableData.tsx
@@ -1,11 +1,11 @@
 import { useMemo, useState } from "react";
 
 export type SortDirection = 'ascending' | 'descending';
+
 export type SortConfig = {
   key: string;
   direction: SortDirection;
 } | null;
-
 
 export const useSortableData = <T,>(items: T[], config = null) => {
   const [sortConfig, setSortConfig] = useState<SortConfig>(config);
@@ -26,20 +26,25 @@ export const useSortableData = <T,>(items: T[], config = null) => {
     return sortableItems;
   }, [items, sortConfig]);
 
-  const requestSort = (key: string) => {
-    let direction: SortDirection = "ascending";
-    if (
-      sortConfig &&
-      sortConfig.key === key &&
-      sortConfig.direction === "ascending"
-    ) {
-      direction = "descending";
+  const requestSort = (key: string | undefined) => {
+    if (key) {
+      let direction: SortDirection = "ascending";
+      if (
+        sortConfig &&
+        sortConfig.key === key &&
+        sortConfig.direction === "ascending"
+      ) {
+        direction = "descending";
+      }
+      setSortConfig({ key, direction });
     }
-    setSortConfig({ key, direction });
   };
 
-  const getClassNamesFor = (name: string): SortDirection | undefined => {
+  const getClassNamesFor = (name: string | undefined): SortDirection | undefined => {
     if (!sortConfig) {
+      return undefined;
+    }
+    if (!name) {
       return undefined;
     }
     return sortConfig.key === name ? sortConfig.direction : undefined;
