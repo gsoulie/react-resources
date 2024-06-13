1aa5d951d41dd01f662fc5a763a64f670393b6c8
gsoulie <gsoulie@users.noreply.github.com>
2024-06-13 10:09:56 +0200

    Create page.tsx

---
+++ react-cart-popover/app/toolbar/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ToolbarClient } from "./client";
+
+const Toolbar = async () => {
+  return (
+    <header>
+      <ToolbarClient />
+    </header>
+  );
+};
+
+export default Toolbar;
